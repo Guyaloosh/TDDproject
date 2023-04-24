cc3f552ccffd292f33ff6ce1412e3d4ba0375e14
Guyaloosh <guyaloosh@gmail.com>
2023-04-24 17:32:10 +0300

    adding colons to the viewStudent

--- WindowsFormsTdd/Form1.Designer.cs
+++ WindowsFormsTdd/Form1.Designer.cs
@@ -52,13 +52,19 @@ namespace WindowsFormsTdd
             this.SoftwereTextBox = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.StudentView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // AddStudentBtn
             // 
-            this.AddStudentBtn.Location = new System.Drawing.Point(56, 579);
+            this.AddStudentBtn.Location = new System.Drawing.Point(124, 835);
+            this.AddStudentBtn.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.AddStudentBtn.Name = "AddStudentBtn";
-            this.AddStudentBtn.Size = new System.Drawing.Size(168, 44);
+            this.AddStudentBtn.Size = new System.Drawing.Size(336, 85);
             this.AddStudentBtn.TabIndex = 0;
             this.AddStudentBtn.Text = "הוספת סטודנט";
             this.AddStudentBtn.UseVisualStyleBackColor = true;
@@ -66,188 +72,241 @@ namespace WindowsFormsTdd
             // StudentName
             // 
             this.StudentName.AutoSize = true;
-            this.StudentName.Location = new System.Drawing.Point(1095, 109);
+            this.StudentName.Location = new System.Drawing.Point(1492, 124);
+            this.StudentName.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.StudentName.Name = "StudentName";
-            this.StudentName.Size = new System.Drawing.Size(51, 13);
+            this.StudentName.Size = new System.Drawing.Size(88, 25);
             this.StudentName.TabIndex = 1;
             this.StudentName.Text = "שם פרטי";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(911, 106);
+            this.textBox1.Location = new System.Drawing.Point(1124, 118);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.Size = new System.Drawing.Size(316, 31);
             this.textBox1.TabIndex = 2;
             // 
             // LastNameTxtBox
             // 
-            this.LastNameTxtBox.Location = new System.Drawing.Point(911, 146);
+            this.LastNameTxtBox.Location = new System.Drawing.Point(1124, 195);
+            this.LastNameTxtBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.LastNameTxtBox.Name = "LastNameTxtBox";
-            this.LastNameTxtBox.Size = new System.Drawing.Size(160, 20);
+            this.LastNameTxtBox.Size = new System.Drawing.Size(316, 31);
             this.LastNameTxtBox.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1095, 149);
+            this.label1.Location = new System.Drawing.Point(1492, 201);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.Size = new System.Drawing.Size(110, 25);
             this.label1.TabIndex = 4;
             this.label1.Text = "שם משפחה";
             // 
             // IdTextBox
             // 
-            this.IdTextBox.Location = new System.Drawing.Point(911, 186);
+            this.IdTextBox.Location = new System.Drawing.Point(1124, 272);
+            this.IdTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.IdTextBox.Name = "IdTextBox";
-            this.IdTextBox.Size = new System.Drawing.Size(160, 20);
+            this.IdTextBox.Size = new System.Drawing.Size(316, 31);
             this.IdTextBox.TabIndex = 5;
             // 
             // IdLabel
             // 
             this.IdLabel.AutoSize = true;
-            this.IdLabel.Location = new System.Drawing.Point(1091, 189);
+            this.IdLabel.Location = new System.Drawing.Point(1484, 277);
+            this.IdLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.IdLabel.Name = "IdLabel";
-            this.IdLabel.Size = new System.Drawing.Size(67, 13);
+            this.IdLabel.Size = new System.Drawing.Size(116, 25);
             this.IdLabel.TabIndex = 6;
             this.IdLabel.Text = "תעודת זהות";
             // 
             // EmailTextBox
             // 
-            this.EmailTextBox.Location = new System.Drawing.Point(911, 226);
+            this.EmailTextBox.Location = new System.Drawing.Point(1124, 349);
+            this.EmailTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.EmailTextBox.Name = "EmailTextBox";
-            this.EmailTextBox.Size = new System.Drawing.Size(160, 20);
+            this.EmailTextBox.Size = new System.Drawing.Size(316, 31);
             this.EmailTextBox.TabIndex = 7;
             // 
             // EmailTextLabel
             // 
             this.EmailTextLabel.AutoSize = true;
-            this.EmailTextLabel.Location = new System.Drawing.Point(1095, 229);
+            this.EmailTextLabel.Location = new System.Drawing.Point(1492, 354);
+            this.EmailTextLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.EmailTextLabel.Name = "EmailTextLabel";
-            this.EmailTextLabel.Size = new System.Drawing.Size(67, 13);
+            this.EmailTextLabel.Size = new System.Drawing.Size(112, 25);
             this.EmailTextLabel.TabIndex = 8;
             this.EmailTextLabel.Text = "כתובת מייל";
             // 
             // PhoneNumTextBox
             // 
-            this.PhoneNumTextBox.Location = new System.Drawing.Point(911, 266);
+            this.PhoneNumTextBox.Location = new System.Drawing.Point(1124, 426);
+            this.PhoneNumTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.PhoneNumTextBox.Name = "PhoneNumTextBox";
-            this.PhoneNumTextBox.Size = new System.Drawing.Size(160, 20);
+            this.PhoneNumTextBox.Size = new System.Drawing.Size(316, 31);
             this.PhoneNumTextBox.TabIndex = 9;
             // 
             // PhoneLabel
             // 
             this.PhoneLabel.AutoSize = true;
-            this.PhoneLabel.Location = new System.Drawing.Point(1091, 269);
+            this.PhoneLabel.Location = new System.Drawing.Point(1484, 431);
+            this.PhoneLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.PhoneLabel.Name = "PhoneLabel";
-            this.PhoneLabel.Size = new System.Drawing.Size(76, 13);
+            this.PhoneLabel.Size = new System.Drawing.Size(129, 25);
             this.PhoneLabel.TabIndex = 10;
             this.PhoneLabel.Text = "מספר פלאפון";
             // 
             // GradesLabels
             // 
             this.GradesLabels.AutoSize = true;
-            this.GradesLabels.Location = new System.Drawing.Point(1092, 319);
+            this.GradesLabels.Location = new System.Drawing.Point(1478, 500);
+            this.GradesLabels.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.GradesLabels.Name = "GradesLabels";
-            this.GradesLabels.Size = new System.Drawing.Size(82, 13);
+            this.GradesLabels.Size = new System.Drawing.Size(135, 25);
             this.GradesLabels.TabIndex = 11;
             this.GradesLabels.Text = "ציוני הסטודנט";
             this.GradesLabels.Click += new System.EventHandler(this.GradesLabels_Click);
             // 
             // MathTextBox
             // 
-            this.MathTextBox.Location = new System.Drawing.Point(956, 349);
+            this.MathTextBox.Location = new System.Drawing.Point(1144, 521);
+            this.MathTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.MathTextBox.Name = "MathTextBox";
-            this.MathTextBox.Size = new System.Drawing.Size(50, 20);
+            this.MathTextBox.Size = new System.Drawing.Size(96, 31);
             this.MathTextBox.TabIndex = 12;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1029, 352);
+            this.label2.Location = new System.Drawing.Point(1290, 527);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.Size = new System.Drawing.Size(96, 25);
             this.label2.TabIndex = 13;
             this.label2.Text = "מתמטיקה";
             // 
             // EnglishTextBox
             // 
-            this.EnglishTextBox.Location = new System.Drawing.Point(956, 389);
+            this.EnglishTextBox.Location = new System.Drawing.Point(1144, 598);
+            this.EnglishTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.EnglishTextBox.Name = "EnglishTextBox";
-            this.EnglishTextBox.Size = new System.Drawing.Size(50, 20);
+            this.EnglishTextBox.Size = new System.Drawing.Size(96, 31);
             this.EnglishTextBox.TabIndex = 14;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1029, 392);
+            this.label3.Location = new System.Drawing.Point(1290, 604);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.Size = new System.Drawing.Size(72, 25);
             this.label3.TabIndex = 15;
             this.label3.Text = "אנגלית";
             // 
             // HebrewTextBox
             // 
-            this.HebrewTextBox.Location = new System.Drawing.Point(956, 429);
+            this.HebrewTextBox.Location = new System.Drawing.Point(1144, 675);
+            this.HebrewTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.HebrewTextBox.Name = "HebrewTextBox";
-            this.HebrewTextBox.Size = new System.Drawing.Size(50, 20);
+            this.HebrewTextBox.Size = new System.Drawing.Size(96, 31);
             this.HebrewTextBox.TabIndex = 16;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(1032, 432);
+            this.label4.Location = new System.Drawing.Point(1296, 681);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.Size = new System.Drawing.Size(51, 25);
             this.label4.TabIndex = 17;
             this.label4.Text = "לשון";
             // 
             // PhysicsTextBox
             // 
-            this.PhysicsTextBox.Location = new System.Drawing.Point(956, 469);
+            this.PhysicsTextBox.Location = new System.Drawing.Point(1144, 752);
+            this.PhysicsTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.PhysicsTextBox.Name = "PhysicsTextBox";
-            this.PhysicsTextBox.Size = new System.Drawing.Size(50, 20);
+            this.PhysicsTextBox.Size = new System.Drawing.Size(96, 31);
             this.PhysicsTextBox.TabIndex = 18;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(1029, 472);
+            this.label5.Location = new System.Drawing.Point(1290, 758);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.Size = new System.Drawing.Size(71, 25);
             this.label5.TabIndex = 19;
             this.label5.Text = "פיזיקה";
             // 
             // SoftwereTextBox
             // 
-            this.SoftwereTextBox.Location = new System.Drawing.Point(956, 509);
+            this.SoftwereTextBox.Location = new System.Drawing.Point(1144, 829);
+            this.SoftwereTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.SoftwereTextBox.Name = "SoftwereTextBox";
-            this.SoftwereTextBox.Size = new System.Drawing.Size(50, 20);
+            this.SoftwereTextBox.Size = new System.Drawing.Size(96, 31);
             this.SoftwereTextBox.TabIndex = 20;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(1032, 512);
+            this.label6.Location = new System.Drawing.Point(1296, 835);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.Size = new System.Drawing.Size(63, 25);
             this.label6.TabIndex = 21;
             this.label6.Text = "תכנות";
             // 
             // StudentView1
             // 
+            this.StudentView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
             this.StudentView1.HideSelection = false;
-            this.StudentView1.Location = new System.Drawing.Point(56, 48);
+            this.StudentView1.Location = new System.Drawing.Point(87, 118);
+            this.StudentView1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.StudentView1.Name = "StudentView1";
-            this.StudentView1.Size = new System.Drawing.Size(573, 500);
+            this.StudentView1.Size = new System.Drawing.Size(969, 665);
             this.StudentView1.TabIndex = 22;
             this.StudentView1.UseCompatibleStateImageBehavior = false;
+            this.StudentView1.View = System.Windows.Forms.View.Details;
             this.StudentView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ממוצע";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "טלפון";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "אימייל";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "תעודת זהות";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "שם מלא";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // FirstNameTxtBox
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1247, 660);
+            this.ClientSize = new System.Drawing.Size(1753, 1034);
             this.Controls.Add(this.StudentView1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.SoftwereTextBox);
@@ -271,6 +330,8 @@ namespace WindowsFormsTdd
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.StudentName);
             this.Controls.Add(this.AddStudentBtn);
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "FirstNameTxtBox";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -304,6 +365,11 @@ namespace WindowsFormsTdd
         private System.Windows.Forms.TextBox SoftwereTextBox;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.ListView StudentView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
     }
 }
 
